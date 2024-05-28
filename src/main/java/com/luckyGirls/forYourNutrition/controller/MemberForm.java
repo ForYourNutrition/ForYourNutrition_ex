72258863a72d0688b2bc8267fda799dd6c13d2dc
NamYeonW00 <cocos121@naver.com>
2024-05-28 23:07:04 +0900

    :recycle: refactor: 회원가입, 로그인 기능 관련 파일 수정

--- src/main/java/com/luckyGirls/forYourNutrition/controller/MemberForm.java
+++ src/main/java/com/luckyGirls/forYourNutrition/controller/MemberForm.java
@@ -1,80 +1,42 @@
 package com.luckyGirls.forYourNutrition.controller;
 
-import java.util.Date;
+import java.io.Serializable;
 
-public class MemberForm {
-	private String id;
-	private String password;
-	private String passwordCheck;
-	private String name;
-	private String nickname;
-	private Date birth;
-	private int gender;
-	private String phone_number;
-	private String email;
-	private int point;
-	private int taking_time;
-  
-	/* JavaBeans Properties */
-  
-	public String getId() {
-		return id;
+import com.luckyGirls.forYourNutrition.domain.Member;
+
+
+@SuppressWarnings("serial")
+public class MemberForm implements Serializable {
+
+	private Member member;
+
+	private boolean newMember;
+
+	private String repeatedPassword;
+
+	public MemberForm(Member account) {
+		this.member = account;
+		this.newMember = false;
 	}
-	public void setId(String id) {
-		this.id = id;
+
+	public MemberForm() {
+		this.member = new Member();
+		this.newMember = true;
 	}
-	public String getPassword() {
-		return password;
+
+	public Member getMember() {
+		return member;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+
+	public boolean isNewMember() {
+		return newMember;
 	}
-	public String getName() {
-		return name;
+
+	public void setRepeatedPassword(String repeatedPassword) {
+		this.repeatedPassword = repeatedPassword;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public String getRepeatedPassword() {
+		return repeatedPassword;
 	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	public Date getBirth() {
-		return birth;
-	}
-	public void setBirth(Date birth) {
-		this.birth = birth;
-	}
-	public int getGender() {
-		return gender;
-	}
-	public void setGender(int gender) {
-		this.gender = gender;
-	}
-	public String getPhone_number() {
-		return phone_number;
-	}
-	public void setPhone_number(String phone_number) {
-		this.phone_number = phone_number;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getPoint() {
-		return point;
-	}
-	public void setPoint(int point) {
-		this.point = point;
-	}
-	public int getTaking_time() {
-		return taking_time;
-	}
-	public void setTaking_time(int taking_time) {
-		this.taking_time = taking_time;
-	} 
 }
